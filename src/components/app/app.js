1c7cdf0442911f79fe750a66e5dfa54405d68e22
Dmitriy Solovev <dmptts@gmail.com>
2021-02-13 00:17:11 +0300

    Настраивает маршрутизацию по страницам

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Main from '../main/main';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Offer from '../offer/offer';
+import NotFound from '../not-found/not-found';
 
 const App = (props) => {
   const {placeCardCount} = props;
 
-  return <Main placeCardCount={placeCardCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main placeCardCount={placeCardCount} />;
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
+        <Route path="/offer/:id?" exact>
+          <Offer />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
