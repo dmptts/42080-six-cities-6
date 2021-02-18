096a763d775eb978a7709c0c2e08f55cd05df60f
Dmitriy Solovev <dmptts@gmail.com>
2021-02-18 22:55:12 +0300

    Подготавливает компонент карточки предложения к получению данных

--- src/components/offer-card/offer-card.js
+++ src/components/offer-card/offer-card.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PlaceCard = () => {
+const OfferCard = ({offer}) => {
+  const {title, previewImage, price, isPremium, type, rating} = offer;
+
   return (
     <React.Fragment>
       <article className="cities__place-card place-card">
-        <div className="place-card__mark">
+        {isPremium ? <div className="place-card__mark">
           <span>Premium</span>
-        </div>
+        </div> : ``}
         <div className="cities__image-wrapper place-card__image-wrapper">
           <a href="#">
-            <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+            <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
           </a>
         </div>
         <div className="place-card__info">
           <div className="place-card__price-wrapper">
             <div className="place-card__price">
-              <b className="place-card__price-value">&euro;120</b>
+              <b className="place-card__price-value">&euro;{price}</b>
               <span className="place-card__price-text">&#47;&nbsp;night</span>
             </div>
             <button className="place-card__bookmark-button button" type="button">
@@ -27,18 +30,22 @@ const PlaceCard = () => {
           </div>
           <div className="place-card__rating rating">
             <div className="place-card__stars rating__stars">
-              <span style={{width: `80%`}}></span>
+              <span style={{width: Math.round(rating) * 20 + `%`}}></span>
               <span className="visually-hidden">Rating</span>
             </div>
           </div>
           <h2 className="place-card__name">
-            <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+            <a href="#">{title}</a>
           </h2>
-          <p className="place-card__type">Apartment</p>
+          <p className="place-card__type">{type[0].toUpperCase() + type.slice(1)}</p>
         </div>
       </article>
     </React.Fragment>
   );
 };
 
-export default PlaceCard;
+OfferCard.propTypes = {
+  offer: PropTypes.object.isRequired
+};
+
+export default OfferCard;
