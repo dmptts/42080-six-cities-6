c238b13839bc6b8d13e8865206986103d0ef74e5
Dmitriy Solovev <dmptts@gmail.com>
2021-03-14 15:50:30 +0300

    Реализует изменение состояния при клике по элементам списка городов

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -5,12 +5,13 @@ import {ActionCreator} from '../../store/actions';
 import citiesPropTypes from './cities-list.prop';
 
 const CitiesList = (props) => {
-  const {cities, currentCity} = props;
+  const {cities, currentCity, onCityClick} = props;
 
   return <ul className={`locations__list tabs__list`}>
     {cities.map((city) => <li
       key={city}
       className="locations__item"
+      onClick={() => onCityClick(city)}
     >
       <a className={`locations__item-link tabs__item + ${currentCity === city ? ` tabs__item--active` : ``}`}>
         <span>{city}</span>
@@ -22,7 +23,8 @@ const CitiesList = (props) => {
 
 CitiesList.propTypes = {
   cities: citiesPropTypes,
-  currentCity: PropTypes.string.isRequired
+  currentCity: PropTypes.string.isRequired,
+  onCityClick: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
@@ -34,7 +36,6 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(ActionCreator.changeCity(city));
   }
 });
-
 
 export {CitiesList};
 export default connect(mapStateToProps, mapDispatchToProps)(CitiesList);
